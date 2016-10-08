71cec86b3b46c3dba0746f07dacce7e13be1c949
Matthew Huebert <no-email@gmail.com>
2016-10-08 17:19:00 +0200

    Move dynamic vars to re-db.core

--- src/re_db/d.clj
+++ src/re_db/d.clj
@@ -1,19 +1,15 @@
 (ns re-db.d)
 
-(def *db-log* 're-db.core/*db-log*)
-(def *db* 're-db.d/*db*)
-(def *mute* 're-db.core/*mute*)
-
 (defmacro branch [db & body]
   `(let [db-before# ~db]
-     (binding [~*db* (~'atom db-before#)
-               ~*mute* true
-               ~*db-log* (~'atom (or (~'some-> ~*db-log* ~'deref)
-                                     {:db-before db-before#
-                                      :db-after  db-before#
-                                      :reports   {}}))]
+     (binding [~'re-db.d/*db* (~'atom db-before#)
+               ~'re-db.core/*mute* true
+               ~'re-db.core/*db-log* (~'atom (or (~'some-> ~'re-db.core/*db-log* ~'deref)
+                                                 {:db-before db-before#
+                                                  :db-after  db-before#
+                                                  :reports   {}}))]
        (do ~@body)
-       @~*db-log*)))
+       @~'re-db.core/*db-log*)))
 
 (defmacro try-branch [db on-error & body]
   `(let [db-before# ~db]
