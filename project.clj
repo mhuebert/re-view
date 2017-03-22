e1d9ed1f99c228fed1eedf5d70781f3df813924c
Matthew Huebert <no-email@gmail.com>
2017-03-22 17:57:26 +0100

    lein-release plugin: bumped version from 0.3.0 to 0.3.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.mhuebert/re-view "0.3.0"
+(defproject org.clojars.mhuebert/re-view "0.3.1-SNAPSHOT"
   :description "Tiny React wrapper"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/clojurescript "1.9.293"]
-                 [org.clojars.mhuebert/re-db "0.1.7-SNAPSHOT"]
+                 [org.clojars.mhuebert/re-db "0.1.7"]
                  [org.clojure/core.match "0.3.0-alpha4"]]
 
   :plugins [[lein-cljsbuild "1.1.2"]
