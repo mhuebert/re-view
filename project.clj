f01707f48f3c95109de973477810603c6152f1c0
Matthew Huebert <no-email@gmail.com>
2017-03-29 12:58:09 +0200

    lein-release plugin: bumped version from 0.1.8 to 0.1.9-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mhuebert/re-db "0.1.8"
+(defproject org.clojars.mhuebert/re-db "0.1.9-SNAPSHOT"
   :description "Tiny data store"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
