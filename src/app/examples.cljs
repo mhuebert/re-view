097eca138df52a40525d56dcda01b6aabc5dd589
Matthew Huebert <no-email@gmail.com>
2017-04-25 23:52:13 +0200

    First launch

--- src/app/examples.cljs
+++ src/app/examples.cljs
@@ -8,7 +8,8 @@
             [clojure.string :as string]
             [re-view-material.core :as ui]
             [clojure.set :as set]
-            [re-db.d :as d]))
+            [re-db.d :as d]
+            [re-view-material.util :as util]))
 
 (defn table-of-contents [toc]
   (ui/PermanentDrawer
@@ -41,35 +42,53 @@
                   (swap! state assoc :toc toc)))}
   [{:keys [view/state]}]
 
-  [:.flex-auto.pa3
-   {:className (if (d/get :ui :theme/dark) "mdc-theme--dark bg-mid-gray white" "bg-near-white")}
+  [:.mdc-typography
+   (util/html-props {:classes (if (d/get :ui :theme/dark)
+                                ["mdc-theme--dark" "bg-mid-gray" "white"]
+                                ["bg-near-white"])
+                     :style   {:minHeight "100%"}})
+   [:.f1.pv4.pt5.fw2.tc.serif.i "Re-View Material"]
+   [:.ph4.pv3.mv3.flex.items-center
+    {:className (str "mdc-theme--dark" (if (d/get :ui :theme/dark) " mdc-theme--accent-bg  " " mdc-theme--primary-bg"))}
+    (-> icons/Search
+        (icons/class "o-50 mr2"))
+    (ui/Input {:placeholder "Search"
+               :value       (:search @state)
+               :className   "mr3 w-100 bn outline-0 pv2 bg-transparent"
+               :autoFocus   true
+               :onChange    #(swap! state assoc :search (.. % -target -value))})
 
-   [:.f1.pt5.fw2.tc.serif.i "Re-View Material"]
-   [:.br2.pa3.mv3
-    (ui/Checkbox {:name     :theme
-                  :checked  (boolean (:theme/dark @state))
-                  :onChange #(d/transact! [[:db/update-attr :ui :theme/dark not]])
-                  :label    "Dark theme"})]
+    (ui/Checkbox {:name      :theme
+                  :checked   (d/get :ui :theme/dark)
+                  :onChange  #(d/transact! [[:db/update-attr :ui :theme/dark not]])
+                  :className "flex-none"
+                  :label     "Dark theme"})]
 
-   [:div.br2.mt2.flex.justify-between.items-stretch.flex-wrap
-    (for [{:keys [label prop-atom component children wrap custom-view]} (concat material-example/examples-data
-                                                                                prosemirror-example/examples-data)]
+   [:div.br2.ma3.flex.justify-between.items-stretch.flex-wrap
+    (for [{:keys [label prop-atom component children wrap custom-view]} (cond->> (concat material-example/examples-data
+                                                                                         prosemirror-example/examples-data)
+                                                                                 (util/ensure-str (:search @state))
+                                                                                 (filter (let [search (string/lower-case (:search @state))]
+                                                                                           #(string/starts-with?
+                                                                                              (string/lower-case (:label %))
+                                                                                              search))))]
       ((view {:key label}
              [{:keys [view/state]}]
              (let [{:keys [edit-mode]} @state]
                [:.ma3.bw2.bb.dib.flex-auto.flex.flex-column.shadow-4.br2
                 {:className (str (if (d/get :ui :theme/dark) "bg-dark-gray b--gray" "bg-white b--light-gray"))}
-                (if prop-atom
-                  (cond-> [:.flex.w-100.pa3.overflow-hidden
-                           {:className "mdc-ripple-surface pointer"
-                            :onClick   #(swap! state update :edit-mode not)}
-                           label
-                           [:.flex-auto]
-                           (if edit-mode
-                             icons/ExpandLess icons/ExpandMore)]
-                          prop-atom (ui/Ripple))
-                  [:.pa3 label])
-                [:.flex.items-center.flex-auto.pv3.ph4
+
+                [:.flex.w-100.pa3.pb0.nb3.items-center
+                 label
+                 [:.flex-auto]
+
+                 (when prop-atom
+                   [:.pointer.na2
+                    {:onClick #(swap! state update :edit-mode not)}
+                    (-> (if edit-mode
+                          icons/ExpandLess icons/ExpandMore)
+                        (assoc-in [1 :className] " pa2 br-pill hover-o-100 o-50"))])]
+                [:.flex.items-center.flex-auto.pa4
                  [:.flex-auto]
                  (if custom-view (custom-view)
                                  (try (cond-> (apply h/with-prop-atom* component prop-atom children)
@@ -77,6 +96,6 @@
                                       (catch js/Error e "Error")))
                  [:.flex-auto]]
                 (when edit-mode
-                  [:.ph3
+                  [:.ph3.b--darken-2.bt.bg-darken-1
                    (or (some-> prop-atom (h/state-editor))
                        [:.f6.o-60.tc.pv3 "No props"])])]))))]])
