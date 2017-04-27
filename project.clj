09ac7faf1d5858ecc18c68ba24eef04aa4c0367f
Matthew Huebert <no-email@gmail.com>
2017-04-27 21:51:18 +0200

    lein-release plugin: preparing 0.3.10 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mhuebert/re-view "0.3.10-SNAPSHOT"
+(defproject org.clojars.mhuebert/re-view "0.3.10"
   :description "Tiny React wrapper"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
