919aa30f25c7192ae950f345f0c454a5aeb97bf4
Matthew Huebert <no-email@gmail.com>
2017-05-01 18:42:39 +0200

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-view-hiccup "0.1.2"
+(defproject re-view-hiccup "0.1.3-SNAPSHOT"
   :description "Hiccup parser for re-view"
 
   :url "https://www.github.com/mhuebert/re-view-hiccup"
