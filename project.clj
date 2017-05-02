4225748e40af1dc8b4fe6cfe80ac0879b72246dc
Matthew Huebert <no-email@gmail.com>
2017-05-02 16:13:37 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-view-material "0.1.0"
+(defproject re-view-material "0.1.1-SNAPSHOT"
   :description "material-design-lite components in re-view"
   :url "https://www.github.com/mhuebert/re-view-material"
   :license {:name "MIT License"
@@ -11,4 +11,5 @@
   :profiles {:provided {:dependencies [[org.clojars.mhuebert/re-view "0.3.9"]]}
              :dev      {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :cljsbuild {:builds []}
+  :lein-release {:deploy-via :clojars}
   :source-paths ["src" "example"])
