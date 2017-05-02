d42367b55eeb7dae0cf92b557de1f3363c2b8e50
Matthew Huebert <no-email@gmail.com>
2017-05-02 14:58:42 +0200

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,31 +1,32 @@
-(defproject re-view-prosemirror "0.1.0"
-            :description "prosemirror editor in re-view"
-            :url "https://www.github.com/mhuebert/re-view-prosemirror"
-            :license {:name "MIT License"
-                      :url  "http://www.opensource.org/licenses/mit-license.php"}
-            :min-lein-version "2.7.1"
-            :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                           [org.clojure/clojurescript "1.9.518"]
-                           [cljsjs/markdown-it "7.0.0-0"]]
-            :provided {:dependencies [[org.clojars.mhuebert/re-view "0.3.9"]
-                                      [org.clojars.mhuebert/re-view-material "0.1.0-SNAPSHOT"]]}
-            :cljsbuild {:builds []}
-            :plugins [[lein-npm "0.6.2"]]
-            :npm {:dependencies [[prosemirror-view "0.20.3"]
-                                 [prosemirror-state "0.20.0"]
-                                 [prosemirror-keymap "0.20.0"]
-                                 [prosemirror-model "0.20.0"]
-                                 [prosemirror-commands "0.20.0"]
-                                 [prosemirror-history "0.20.0"]
-                                 [prosemirror-markdown "0.20.0"]
-                                 [prosemirror-transform "0.20.0"]
-                                 [prosemirror-schema-list "0.20.0"]
-                                 [prosemirror-inputrules "0.20.0"]
+(defproject re-view-prosemirror "0.1.1"
+  :description "prosemirror editor in re-view"
+  :url "https://www.github.com/mhuebert/re-view-prosemirror"
+  :license {:name "MIT License"
+            :url  "http://www.opensource.org/licenses/mit-license.php"}
+  :min-lein-version "2.7.1"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.518"]
+                 [cljsjs/markdown-it "7.0.0-0"]]
+  :provided {:dependencies [[org.clojars.mhuebert/re-view "0.3.9"]
+                            [org.clojars.mhuebert/re-view-material "0.1.0-SNAPSHOT"]]}
+  :cljsbuild {:builds []}
+  :plugins [[lein-npm "0.6.2"]]
+  :lein-release {:deploy-via :clojars}
+  :npm {:dependencies [[prosemirror-view "0.20.3"]
+                       [prosemirror-state "0.20.0"]
+                       [prosemirror-keymap "0.20.0"]
+                       [prosemirror-model "0.20.0"]
+                       [prosemirror-commands "0.20.0"]
+                       [prosemirror-history "0.20.0"]
+                       [prosemirror-markdown "0.20.0"]
+                       [prosemirror-transform "0.20.0"]
+                       [prosemirror-schema-list "0.20.0"]
+                       [prosemirror-inputrules "0.20.0"]
 
-                                 [rollup-plugin-commonjs "8.0.2"]
-                                 [rollup-plugin-json "2.1.1"]
-                                 [rollup-plugin-node-builtins "2.1.0"]
-                                 [rollup-plugin-node-resolve "3.0.0"]
-                                 [rollup-plugin-uglify "1.0.2"]
-                                 [rollup-watch "3.2.2"]]}
-            :source-paths ["src" "example"])
+                       [rollup-plugin-commonjs "8.0.2"]
+                       [rollup-plugin-json "2.1.1"]
+                       [rollup-plugin-node-builtins "2.1.0"]
+                       [rollup-plugin-node-resolve "3.0.0"]
+                       [rollup-plugin-uglify "1.0.2"]
+                       [rollup-watch "3.2.2"]]}
+  :source-paths ["src" "example"])
