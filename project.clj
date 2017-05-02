bd8dfde67ca99921eb161b100c3dd6d2e101a0f7
Matthew Huebert <no-email@gmail.com>
2017-05-02 16:09:50 +0200

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-view-prosemirror "0.1.2"
+(defproject re-view-prosemirror "0.1.3-SNAPSHOT"
   :description "prosemirror editor in re-view"
   :url "https://www.github.com/mhuebert/re-view-prosemirror"
   :license {:name "MIT License"
