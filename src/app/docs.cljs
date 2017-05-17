3206f4640132d47592152ce331962175e195a1f3
Matthew Huebert <no-email@gmail.com>
2017-05-17 19:56:16 +0200

    docs

--- src/app/docs.cljs
+++ src/app/docs.cljs
@@ -7,7 +7,8 @@
             [clojure.string :as string]
             [goog.events :as events]
             [re-view-routing.core :as routing]
-            [re-db.d :as d]))
+            [re-db.d :as d]
+            [re-view-material.icons :as icons]))
 
 (def cache {})
 
@@ -15,15 +16,20 @@
   (ui/ToolbarSection
     {:class "flex items-center"}
     (ui/ToolbarTitle {:class "no-underline"
-                      :href "/"} "Docs")
+                      :href  "/"} "Docs")
     [:.flex-auto]))
 
-(defn prefix-url [url]
-  (str "http://re-view.github.io/re-view"
-       (str
-         (when-not (string/starts-with? url "/") "/")
-         url
-         (when-not (string/ends-with? url ".md") ".md"))))
+(defn md-path [path]
+  (str
+    (when-not (string/starts-with? path "/") "/")
+    path
+    (when-not (string/ends-with? path ".md") ".md")))
+
+(defn download-url [path]
+  (str "http://re-view.github.io/re-view" (md-path path)))
+
+(defn edit-url [path]
+  (str "http://prose.io/#re-view/re-view/edit/master/docs" (md-path path)))
 
 (defn local-link [el]
   (when-not (= (.-href el) (and (.hasAttribute el "href") (.getAttribute el "href")))
@@ -31,17 +37,17 @@
          (string/join "/")
          (str "/docs/"))))
 #_(defn markdown-link [href]
-  (when (string/ends-with? href ".md")
-    href))
+    (when (string/ends-with? href ".md")
+      href))
 
 (defview markdown-page
   {:initial-state           (fn [_ url] {:value (get cache url)})
    :update                  (fn [{:keys [view/state]} url]
                               (swap! state assoc :loading true)
-                              (xhr/send (prefix-url url) (fn [e]
-                                                           (let [value (.getResponseText (.-target e))]
-                                                             (set! cache (assoc cache url value))
-                                                             (reset! state {:value value})))))
+                              (xhr/send (download-url url) (fn [e]
+                                                             (let [value (.getResponseText (.-target e))]
+                                                               (set! cache (assoc cache url value))
+                                                               (reset! state {:value value})))))
    :intercept               (fn [this e]
                               (when-let [href (some-> (routing/closest (.-target e) routing/link?)
                                                       (local-link))]
@@ -57,7 +63,14 @@
                                 (.update this (first children))))}
   [{:keys [view/state]} url]
   (let [{:keys [value error]} @state]
-    [:.ma4.ph4.pv2.lh-copy.markdown-copy.center.mw7.elevated-card
+    [:.ma4.ph4.pv2.lh-copy.markdown-copy.center.mw7.elevated-card.relative
+     (ui/Button {:href    (edit-url url)
+                 :target "_blank"
+                 :icon    icons/ModeEdit
+                 :label   "Edit on GitHub"
+                 :dense   true
+                 :compact true
+                 :class   "absolute top-0 right-0 ma2"})
      (cond error [:div "Error..." error]
            value [:div
                   [:div (md value)]]
@@ -65,7 +78,8 @@
 
 (defview home
   []
-  [:div (markdown-page "/index.md")])
+  [:div
+   (markdown-page "/index.md")])
 
 (defview page
   [this segments]
