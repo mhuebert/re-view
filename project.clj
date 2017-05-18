9adcf14b93760d2eba7eb699b97560533818f505
Matthew Huebert <no-email@gmail.com>
2017-05-18 19:08:50 +0200

    lein-release plugin: bumped version from 0.3.14 to 0.3.15-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-view "0.3.14"
+(defproject re-view "0.3.15-SNAPSHOT"
   :description "Tiny React wrapper"
   :url "https://www.github.com/re-view/re-view"
   :license {:name "Eclipse Public License"
