787e33b4505a5d4926300de150cd22005d3a3fcf
Matthew Huebert <no-email@gmail.com>
2017-07-24 02:24:15 +0200

    project.clj

--- re-view-template/project.clj
+++ re-view-template/project.clj
@@ -1,6 +1,6 @@
 (defproject re-view/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Template for creating re-view apps."
+  :url "https://www.github.com/re-view/re-view"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
