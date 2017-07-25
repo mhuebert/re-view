d4f1c06fd6a00b745fad9eb5c4f8a5f31015ab2b
Matthew Huebert <no-email@gmail.com>
2017-07-25 10:37:44 +0200

    lein-release plugin: preparing 0.3.20 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-view "0.3.20-SNAPSHOT"
+(defproject re-view "0.3.20"
   :description "Tiny React wrapper"
   :url "https://www.github.com/re-view/re-view"
   :license {:name "Eclipse Public License"
