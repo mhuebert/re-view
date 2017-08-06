acc5b01a81a610da35a535d38bc9bd5bc3e50fcc
Matthew Huebert <no-email@gmail.com>
2017-08-07 00:05:22 +0200

    Versions

--- re_view_material/project.clj
+++ re_view_material/project.clj
@@ -1,4 +1,4 @@
-(defproject re-view-material "0.1.6-SNAPSHOT"
+(defproject re-view-material "0.1.6"
   :description "Material design components in re-view"
   :url "https://www.github.com/braintripping/re-view/tree/master/re_view_material"
   :license {:name "MIT License"
