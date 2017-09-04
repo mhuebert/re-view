7f835a5908ee11965c43172c3620686d7533dfa3
Matthew Huebert <no-email@gmail.com>
2017-09-04 12:07:30 +0200

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- re_view_hiccup/project.clj
+++ re_view_hiccup/project.clj
@@ -1,4 +1,4 @@
-(defproject re-view-hiccup "0.1.10"
+(defproject re-view-hiccup "0.1.11-SNAPSHOT"
   :description "Hiccup parser for re-view"
 
   :url "https://www.github.com/braintripping/re-view/tree/master/re_view_hiccup"
