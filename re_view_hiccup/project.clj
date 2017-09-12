cf545248e64755d316ee352f6667b0e18b45e00b
Matthew Huebert <no-email@gmail.com>
2017-09-12 15:02:12 +0200

    lein-release plugin: preparing 0.1.11 release

--- re_view_hiccup/project.clj
+++ re_view_hiccup/project.clj
@@ -1,4 +1,4 @@
-(defproject re-view-hiccup "0.1.11-SNAPSHOT"
+(defproject re-view-hiccup "0.1.11"
   :description "Hiccup parser for re-view"
 
   :url "https://www.github.com/braintripping/re-view/tree/master/re_view_hiccup"
