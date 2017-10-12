cb8dc58cccd16b437afa9836650baf634a5bf038
Matthew Huebert <no-email@gmail.com>
2017-10-12 15:50:34 +0200

    lein-release plugin: preparing 0.3.33 release

--- re_view/project.clj
+++ re_view/project.clj
@@ -1,12 +1,12 @@
-(defproject re-view "0.3.32-SNAPSHOT"
+(defproject re-view "0.3.33"
   :description "Tiny React wrapper"
   :url "https://www.github.com/braintripping/re-view/tree/master/re_view"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.854"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [re-db "0.1.12"]
-                 [re-view-hiccup "0.1.12"]]
+                 [re-view-hiccup "0.1.14"]]
 
   :plugins [[lein-cljsbuild "1.1.2"]
             #_[lein-figwheel "0.5.0-2"]
@@ -38,14 +38,4 @@
                                        :asset-path    "/base/resources/public/js/test"
                                        :main          tests.runner
                                        :infer-externs true
-                                       :optimizations :none}}]}
-  :profiles {:provided {:dependencies [[cljsjs/react-dom "16.0.0-beta.2-0"]
-                                       [cljsjs/react "16.0.0-beta.2-0"]
-                                       [cljsjs/react-dom-server "16.0.0-beta.2-0"]]}
-             :dev      {:dependencies [[cljsjs/react-dom "16.0.0-beta.2-0"]
-                                       [cljsjs/react-dom-server "16.0.0-beta.2-0"]
-                                       [org.clojure/test.check "0.9.0"]]}
-             :test     {:dependencies [[cljsjs/react-dom "16.0.0-beta.2-0"]
-                                       [cljsjs/react "16.0.0-beta.2-0"]
-                                       [cljsjs/react-dom-server "16.0.0-beta.2-0"]
-                                       [org.clojure/test.check "0.9.0"]]}})
+                                       :optimizations :none}}]})
