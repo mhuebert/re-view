5774be97ae56a06c0629cd40bf6b4ea8bc86add4
Matthew Huebert <no-email@gmail.com>
2017-10-24 18:38:15 +0200

    Fix macros, borked by the infamous ClojureScript syntax quote

--- re-frame-simple/src/re_view/re_frame_simple.clj
+++ re-frame-simple/src/re_view/re_frame_simple.clj
@@ -13,31 +13,19 @@
        (fn [~(or db-sym '_) ~(into '[_] arg-syms)]
          ~@body))))
 
-(defmacro defupdate-fx
-  "Registers an event handler with re-frame which may return an update
-  to the db as well as other coeffects.
-
-  The function will be passed a _map_ containing `db`, eg. {:db db},
-  and should return a map containing :db as well as any effects."
-  [& args]
-  (let [[docstring [event-key [coeffects-sym & arg-syms] & body]] (parse-opt-args [string?] args)]
-    `(~'re-frame.core/reg-event-fx
-       ~event-key
-       (fn [~(or coeffects-sym '_) ~(into '[_] arg-syms)]
-         ~@body))))
-
 (defmacro defquery
   "Registers a subscription with re-frame."
-  [name argslist & body]
-  (let [subscription-key (keyword (str *ns*) (str name))
+  [name & args]
+  (let [[docstring [argslist & body]] (parse-opt-args [string?] args)
+        subscription-key (keyword (str *ns*) (str name))
         arg-syms argslist]
     `(def ~name
-       (do
-         (re-frame.core/reg-sub
-           ~subscription-key
-           (fn [_ ~(into '[_] arg-syms)]
-             (binding [~'re-view.re-frame-simple/*in-query?* true]
-               ~@body)))
-         (fn [& args#]
-           @(re-frame.core/subscribe
-              (into [~subscription-key] args#)))))))
+      (do
+        (re-frame.core/reg-sub
+          ~subscription-key
+          (fn [~'_ ~(into '[_] arg-syms)]
+            (binding [~'re-view.re-frame-simple/*in-query?* true]
+              ~@body)))
+        (fn [& args#]
+          @(re-frame.core/subscribe
+             (into [~subscription-key] args#)))))))
