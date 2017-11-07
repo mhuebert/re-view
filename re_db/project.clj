cf837455a827c89722e647d6201cf8e4ba223bff
Matthew Huebert <no-email@gmail.com>
2017-11-07 18:21:31 +0100

    lein-release plugin: bumped version from 0.1.14 to 0.1.15-SNAPSHOT for next development cycle

--- re_db/project.clj
+++ re_db/project.clj
@@ -1,4 +1,4 @@
-(defproject re-db "0.1.14"
+(defproject re-db "0.1.15-SNAPSHOT"
   :description "in-memory javascript key-value store inspired by Datomic and DataScript"
   :url "https://www.github.com/braintripping/re-view/tree/master/re_db"
   :license {:name "Eclipse Public License"
